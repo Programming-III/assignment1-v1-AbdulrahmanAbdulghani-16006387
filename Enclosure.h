3019cee903c497df843d420dacd1248be914141a
AbdulrahmanAbdulghani-16006387 <abdulrahman.ahmed@student.giu-uni.de>
2025-11-12 15:52:36 +0200

    Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,13 +3,41 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
 
-
-
-
-
-
-
+class Enclosure {
+    private:
+    Animal* animal;
+    int capacity;
+    int currentCount;
+    
+    public:
+    Enclosure() {
+        animal = nullptr;
+        capacity = 0;
+        currentCount = 0;
+    }
+    
+    Enclosure(Animal* a, int c, int cc){
+        animal = a;
+        capacity = c;
+        currentCount = cc;
+    }
+    
+    
+    ~Enclosure(){
+        delete [] animal;
+    }
+    
+    void addAnimal(Animal& a) {
+        if(currentCount<capacity){
+            animal[currentCount++] = a;
+        }}
+        
+    void displayAnimals() {
+        for(int i = 0; i<currentCount; i++){
+            animal[i].display();
+        }
+    }
+};
 
 #endif
