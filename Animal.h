d2320737e951dac35aaec601c8b750157c5429ac
AbdulrahmanAbdulghani-16006387 <abdulrahman.ahmed@student.giu-uni.de>
2025-11-12 15:52:13 +0200

    Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,43 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
 
+class Animal{
+    
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    Animal(){
+        name = "Lion";
+        age = 0;
+        isHungry = false;
+    }
+    
+    Animal(string s, int a, bool i){
+        name = s;
+        age = a;
+        isHungry = i;
+    }
+    
+    ~Animal(){}
+    
+    virtual void display(){
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "isHungry: " << isHungry << endl;
+    }
+    
+    void feed(){
+        cout << isHungry << endl;
+    }
+    
+};
 
 
 #endif
