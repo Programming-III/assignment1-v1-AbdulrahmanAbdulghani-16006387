f0886dde6f46985db967e4b548d0e474b89529b5
AbdulrahmanAbdulghani-16006387 <abdulrahman.ahmed@student.giu-uni.de>
2025-11-12 15:54:53 +0200

    Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,10 +1,34 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 
+#include <iostream>
 #include <string>
-using namespace std;
 
-//define visitor class here
+using namespace std;
+class Visitor{
+    private: 
+    string visitorName;
+    int ticketsBought;
+    
+    public: 
+    Visitor(){
+        visitorName = "Abdulrahman";
+        ticketsBought = 0;
+    }
+    
+    Visitor(string s, int t){
+        visitorName = s;
+        ticketsBought = t;
+    }
+    
+    ~Visitor() {}
+    
+    void displayInfo(){
+        cout << "Visitor Name: " << visitorName << endl;
+        cout << "Tickets Bought" << ticketsBought << endl;
+    }
+};
+
 
 
 
